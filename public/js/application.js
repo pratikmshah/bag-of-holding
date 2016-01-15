f6ec9f28df0514ad0d2e9aa945a4f3498694d895
Chris Miklius <cmiklius@gmail.com>
2016-01-15 09:59:24 -0600

    add sound after search

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-
+$(".result-container").ready(function() {
+    var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', 'sounds/itemget.mp3');
+        audioElement.play();
 });
+
+$(".errors").load(function(){
+  var audioElement2 = document.createElement('audio');
+        audioElement2.setAttribute('src', 'sounds/ganon.mp3');
+        audioElement2.play();
+})

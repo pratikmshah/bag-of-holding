dcbb97c2b8ce9041392ad3b4ef122177397a79eb
Chris Miklius <cmiklius@gmail.com>
2016-01-14 20:37:05 -0600

    add 404 page and link back home

--- app/controllers/searches.rb
+++ app/controllers/searches.rb
@@ -3,8 +3,12 @@ get '/searches/new' do
 end
 
 post '/searches' do
-  data = generate_response(params[:game_name], params[:console])
-  @y = access_response_result(data)
-
-  erb :'/searches/create'
+  @data = generate_response(params[:game_name], params[:console])
+  p @data
+  if @data != nil && @data.body["message"] == nil
+    @y = access_response_result(@data)
+    erb :'/searches/create'
+  else
+    erb :'/searches/error'
+  end
 end

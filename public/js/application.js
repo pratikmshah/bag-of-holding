4ed3165862574ca7e8505a1f3dfcda2ed1a26670
Chris Miklius <cmiklius@gmail.com>
2016-01-15 10:28:53 -0600

    add sounds for results

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,11 @@
-$(".result-container").ready(function() {
-    var audioElement = document.createElement('audio');
-        audioElement.setAttribute('src', 'sounds/itemget.mp3');
-        audioElement.play();
-});
+// $(".result-container").ready(function() {
+//     var audioElement = document.createElement('audio');
+//         audioElement.setAttribute('src', 'sounds/itemget.mp3');
+//         audioElement.play();
+// });
 
-$(".errors").load(function(){
-  var audioElement2 = document.createElement('audio');
-        audioElement2.setAttribute('src', 'sounds/ganon.mp3');
-        audioElement2.play();
-})
+// $(".errors").load(function(){
+//   var audioElement2 = document.createElement('audio');
+//         audioElement2.setAttribute('src', 'sounds/ganon.mp3');
+//         audioElement2.play();
+// })
